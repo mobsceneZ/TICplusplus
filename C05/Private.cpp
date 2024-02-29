23f6abefc5682d3dd852e7281f86a65676f78c5c
Lingming Zhang <zjuzlm01@gmail.com>
2024-02-29 14:49:19 +0800

    first commit

---
+++ C05/Private.cpp
@@ -0,0 +1,19 @@
+struct B {
+private:
+    char j;
+    float f;
+public:
+    int i;
+    void func();
+};
+
+void B::func() {
+    i = 0;
+    j = '0';
+    f = 0.0;
+}
+
+int main() {
+    B b;
+    b.i = 1;
+}
